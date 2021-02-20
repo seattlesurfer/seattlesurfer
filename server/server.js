77d2ed3c88312e10c753c7651b0460a8e061f0f9
David Duffy <daviduffy1@gmail.com>
2021-02-20 15:18:41 -0800

    fixed robots.txt

--- server/server.js
+++ server/server.js
@@ -4,25 +4,40 @@ const fs = require('fs');
 const save = (data, filename = 'test') => {
   fs.writeFileSync(`${filename}.json`, JSON.stringify(data, null, 2));
 };
-https.get('https://www.ndbc.noaa.gov/data/realtime2/WPOW1.cwind', (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
 
-  let body = '';
+const data = require('../body.json');
 
-  res.on('data', (data) => {
-    body += data;
-  });
+const processData = (body) => {
+  const [pHeadings, colorHeadings, ...pRows] = body.split('\n');
+  const headings = pHeadings.replace(/\s+/g, ',').split(',');
+  const rows = pRows.map(pRow => pRow.replace(/\s+/g, ',').split(','));
 
-  res.on('end', () => {
-    const [pHeadings, colorHeadings, ...pRows] = body.split('\n');
-    const headings = pHeadings.replace(/\s+/g, ',').split(',');
-    const rows = pRows.map(pRow => pRow.replace(/\s+/g, ',').split(','));
+  save({ headings, rows }, 'body');
+  // save(body, 'body');
+};
 
-    save({ headings, rows }, 'body');
-    save(body, 'body');
-  });
-});
+processData(data);
+
+
+// https.get('https://www.ndbc.noaa.gov/data/realtime2/WPOW1.cwind', (res) => {
+//   console.log('statusCode:', res.statusCode);
+//   console.log('headers:', res.headers);
+
+//   let body = '';
+
+//   res.on('data', (data) => {
+//     body += data;
+//   });
+
+//   res.on('end', () => {
+//     const [pHeadings, colorHeadings, ...pRows] = body.split('\n');
+//     const headings = pHeadings.replace(/\s+/g, ',').split(',');
+//     const rows = pRows.map(pRow => pRow.replace(/\s+/g, ',').split(','));
+
+//     save({ headings, rows }, 'body');
+//     // save(body, 'body');
+//   });
+// });
 
 
 
